ae0303035aee5fd8e9a8b8ce207adcc112e11e3f
pavsaiprasad <psaiprasad@wrlonlap74.wrhammersmith.worldremit.com>
2018-02-11 23:19:12 +0000

    feat: Validation - added prop types for type checking

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
 import BooksGrid from './BooksGrid'
 
 class BookShelves extends Component{
+    static propTypes = {
+     books: PropTypes.array.isRequired,
+     onUpdateBookShelf: PropTypes.func.isRequired
+   }
     getBooksBasedOnShelf=(shelf)=>{
         const books = this.props.books;
         return books.filter((book)=> book.shelf === shelf)
@@ -16,15 +21,15 @@ class BookShelves extends Component{
                 <div className="bookshelf">
                     <h2 className="bookshelf-title">Currently Reading</h2>
                     <div className="bookshelf-books">
-                        <BooksGrid books={this.getBooksBasedOnShelf('currentlyReading')} filter='currentlyReading' onUpdateBookShelf={this.props.onUpdateBookShelf}/>
+                        <BooksGrid books={this.getBooksBasedOnShelf('currentlyReading')} onUpdateBookShelf={this.props.onUpdateBookShelf}/>
                     </div>
                     <h2 className="bookshelf-title">Want to Read</h2>
                     <div className="bookshelf-books">
-                        <BooksGrid books={this.getBooksBasedOnShelf('wantToRead')} filter='wantToRead' onUpdateBookShelf={this.props.onUpdateBookShelf}/>
+                        <BooksGrid books={this.getBooksBasedOnShelf('wantToRead')} onUpdateBookShelf={this.props.onUpdateBookShelf}/>
                     </div>
                     <h2 className="bookshelf-title">Read</h2>
                     <div className="bookshelf-books">
-                        <BooksGrid books={this.getBooksBasedOnShelf('read')} filter='read' onUpdateBookShelf={this.props.onUpdateBookShelf}/>
+                        <BooksGrid books={this.getBooksBasedOnShelf('read')} onUpdateBookShelf={this.props.onUpdateBookShelf}/>
                     </div>
                 </div>
                 <div className="open-search">
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
 import * as BooksAPI from './BooksAPI'
 import BooksGrid from './BooksGrid'
 
 class SearchBooks extends Component{
+     static propTypes = {
+     books: PropTypes.array.isRequired,
+     onUpdateBookShelf: PropTypes.func.isRequired
+    }
     state = {
         searchResults:[]
     }
